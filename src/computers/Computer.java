07c0c11768693b029276a47c8d9f7b092c9f3829
Andrii Kovalenko <ccjoke@gmail.com>
2025-06-10 16:01:06 +0200

    Task1 Part2

--- src/computers/Computer.java
+++ src/computers/Computer.java
@@ -14,7 +14,10 @@ public class Computer {
     private ComputerType computerType;// Computer type (e.g., desktop computer, laptop)
     private boolean isPreinstalledOS; // Preinstalled operating system: Yes/No
     private int garantie;             // Garantie in months
+    private double purchasePrice;     // Purchase price in format 0.00
+    private double sellingPrice;      // Selling price in format 0.00
     static Scanner scanner = new Scanner(System.in);
+
     //endregion
 
     //region Constructors
@@ -29,6 +32,9 @@ public class Computer {
         this.screenSize = 24;
         this.isPreinstalledOS = true;
         this.garantie = 36;
+        this.purchasePrice = 400.00;
+        this.sellingPrice = 600.00;
+
     }
 
     //Constructor with the minimum values of manufacturer name, screen size,
@@ -43,6 +49,8 @@ public class Computer {
         this.ramMemory = 16;
         this.numberUSBPorts = 4;
         this.garantie = 36;
+        this.purchasePrice = 500.00;
+        this.sellingPrice = 800.00;
     }
 
     //Constructor requiring all values
@@ -58,6 +66,8 @@ public class Computer {
         this.ramMemory = ramMemory;
         this.numberUSBPorts = numberUSBPorts;
         this.garantie = garantie;
+        this.purchasePrice = 600.00;
+        this.sellingPrice = 1000.00;
     }
     //endregion
 
@@ -133,45 +143,23 @@ public class Computer {
     public void setGarantie(int garantie) {
         this.garantie = garantie;
     }
+
+    public double getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(double purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public double getSellingPrice() {
+        return sellingPrice;
+    }
+
+    public void setSellingPrice(double sellingPrice) {
+        this.sellingPrice = sellingPrice;
+    }
     //endregion
-
-    @Override
-    public String toString() {
-        return "Computer configuration \n"+
-                "   Manufacturer name: " + manufacturerName + "\n" +
-                "   Computer type: " + computerType.showName() + "\n" +
-                "   Processor speed: " + procSpeed + " Mhz\n" +
-                "   Hard drive size: " + hardMemory + " Gb\n" +
-                "   RAM size: " + ramMemory + " Gb\n" +
-                "   Number of USB ports: " + numberUSBPorts + "\n" +
-                "   Screen size: " + screenSize + " inch \n" +
-                "   Preinstalled operating system: Yes/No: " + (isPreinstalledOS ? "Yes" : "No") + "\n" +
-                "   Garantie " + garantie + " months\n";
-    }
-
-    public static Computer createBasicConfiguration(){
-        System.out.println("Please, enter basic parameters yours computer");
-        String manufacturerName = inputManufacturerName();
-        int screenSize = inputScreenSize();
-        boolean isPreinstalledOS = chooseIsPreinstalledOS();
-        return new Computer(manufacturerName, screenSize, isPreinstalledOS);
-    }
-
-    public static Computer createCustomConfiguration(){
-        System.out.println("Please, enter all parameters of your computer");
-        String manufacturerName = inputManufacturerName();
-        int screenSize = inputScreenSize();
-        boolean isPreinstalledOS = chooseIsPreinstalledOS();
-        ComputerType computerType = inputComputerType();
-        int procSpeed = inputProcSpeed();
-        int hardMemory = inputHardMemory();
-        int ramMemory = inputRAMSize();
-        int numberUSBPorts = inputNumberUSB();
-        int garantie = inputGarantie();
-
-        return new Computer(manufacturerName, screenSize, isPreinstalledOS,
-                computerType, procSpeed, hardMemory, ramMemory, numberUSBPorts, garantie);
-    }
 
     //region Static inputs for fields
     public static String inputManufacturerName() {
@@ -266,7 +254,6 @@ public class Computer {
     }
 
     private static ComputerType inputComputerType(Scanner scanner){
-        ComputerType[] types = ComputerType.values();
         do {
             System.out.println("""
                      Please chose correct computer type.
@@ -291,5 +278,96 @@ public class Computer {
             }
         }while (true);
     }
+
     //endregion
+
+    @Override
+    public String toString() {
+        return "Computer configuration \n"+
+                "   Manufacturer name: " + manufacturerName + "\n" +
+                "   Computer type: " + computerType.showName() + "\n" +
+                "   Processor speed: " + procSpeed + " Mhz\n" +
+                "   Hard drive size: " + hardMemory + " Gb\n" +
+                "   RAM size: " + ramMemory + " Gb\n" +
+                "   Number of USB ports: " + numberUSBPorts + "\n" +
+                "   Screen size: " + screenSize + " inch \n" +
+                "   Preinstalled operating system: Yes/No: " + (isPreinstalledOS ? "Yes" : "No") + "\n" +
+                "   Garantie " + garantie + " months\n" +
+                "   Price " + String.format("%.2f",sellingPrice);
+    }
+
+    public static Computer createBasicConfiguration(){
+        System.out.println("Please, enter basic parameters yours computer");
+        String manufacturerName = inputManufacturerName();
+        int screenSize = inputScreenSize();
+        boolean isPreinstalledOS = chooseIsPreinstalledOS();
+        return new Computer(manufacturerName, screenSize, isPreinstalledOS);
+    }
+
+    public static Computer createCustomConfiguration(){
+        System.out.println("Please, enter all parameters of your computer");
+        String manufacturerName = inputManufacturerName();
+        int screenSize = inputScreenSize();
+        boolean isPreinstalledOS = chooseIsPreinstalledOS();
+        ComputerType computerType = inputComputerType();
+        int procSpeed = inputProcSpeed();
+        int hardMemory = inputHardMemory();
+        int ramMemory = inputRAMSize();
+        int numberUSBPorts = inputNumberUSB();
+        int garantie = inputGarantie();
+
+        return new Computer(manufacturerName, screenSize, isPreinstalledOS,
+                computerType, procSpeed, hardMemory, ramMemory, numberUSBPorts, garantie);
+    }
+
+    public double getProfit() {
+        return this.sellingPrice - this.purchasePrice;
+    }
+
+    public void changeCharacteristics(){
+        String menu = """
+                You can choose which characteristic you want to change:
+                Choose operation you want to run:
+                1 - RAM size
+                2 - Hard drive size
+                3 - Screen size
+                4 - Preinstalled OS
+                m - Show menu
+                Any other enter - end the changes
+               \s""";
+
+        System.out.print(menu);
+        Scanner scanner = new Scanner(System.in);
+        String choice;
+        boolean isContinued = true;
+        do {
+            System.out.print("\nYour change choice: ");
+            choice = scanner.nextLine();
+            switch (choice) {
+                case "1":
+                    System.out.println("Current RAM size is: " + this.ramMemory + " Gb");
+                    this.ramMemory = inputRAMSize();
+                    break;
+                case "2":
+                    System.out.println("Current Hard drive is: " + this.hardMemory + " Gb");
+                    this.ramMemory = inputHardMemory();
+                    break;
+                case "3":
+                    System.out.println("Current screen size is: " + this.screenSize + " inches");
+                    this.screenSize = inputScreenSize();
+                    break;
+                case "4":
+                    System.out.println("Current choice of preinstalled OS option: " + (isPreinstalledOS ? "Yes" : "No"));
+                    this.isPreinstalledOS = chooseIsPreinstalledOS();
+                    break;
+                case "m":
+                    System.out.println(menu);
+                    break;
+                default:
+                    isContinued = false;
+                    break;
+            }
+        } while (isContinued);
+    }
+
 }
--- src/computers/ComputerTest.java
+++ src/computers/ComputerTest.java
@@ -7,13 +7,15 @@ public class ComputerTest {
         Scanner scanner = new Scanner(System.in);
         String choice;
         boolean isContinued = true;
-        Computer computer;
+        Computer computer = null;
         String menu = """
                 Computer shop
                 Choose operation you want to run:
                 1 - Choose computer with predefined configuration
                 2 - Choose computer with your own basic parameters
                 3 - Select your own configuration
+                4 - Upgrade characteristics
+                5 - Show profit
                 m - Show menu
                 Any other enter - end the program
                \s""";
@@ -37,6 +39,23 @@ public class ComputerTest {
                     computer = Computer.createCustomConfiguration();
                     System.out.println(computer);
                     break;
+                case "4":
+                    if (computer ==null){
+                        System.out.println("To upgrade characteristics first you have to choose a configuration,\n" +
+                                "you want to upgrade with 1, 2 or 3 punkts of menu");
+                        break;
+                    }
+                    System.out.println("Select your own configuration \n");
+                    computer.changeCharacteristics();
+                    System.out.println(computer);
+                    break;
+                case "5":
+                    if (computer ==null){
+                        System.out.println("To show profit first you have to choose a configuration with 1, 2 or 3 punkts of menu");
+                        break;
+                    }
+                    System.out.println("Profit is: " + String.format("%.2f",computer.getProfit()));
+                   break;
                 case "m":
                     System.out.println(menu);
                     break;
